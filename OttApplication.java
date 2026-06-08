88ea6c9ef6c93114032c4234bfd678df0f27dd50
praveenkondapally <43089438+praveenkondapally@users.noreply.github.com>
2026-06-08 11:34:04 +0530

    Create OttApplication.java

---
+++ OttApplication.java
@@ -0,0 +1,12 @@
+package com.devops.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OttApplication.class, args);
+    }
+}
